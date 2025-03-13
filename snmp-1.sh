18379fb37f4cb6f56d00a3d2fe71b8ba86ad3a4f
PixelGirl20 <105322502+PixelGirl20@users.noreply.github.com>
2025-03-13 10:06:47 +0100

    Create snmp-1.sh
    partie 1

---
+++ snmp-1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Définition des paramètres SNMP en dur
+oid="1.3.6.1.2.1.2.2.1.10.3"  # OID pour ifInOctets de l'interface 3
+agent_ip="192.168.1.1"         # Adresse IP de l'agent SNMP
+community="public"             # Communauté SNMP v2c
+
+# Exécution de la requête SNMP
+value=$(snmpget -v2c -c "$community" "$agent_ip" "$oid" | awk '{print $NF}')
+
+# Affichage du résultat
+echo "Valeur du compteur d’octets de l’interface 3 : ${value}"
