edf0d03a45952afdc419a799c27e735f5ef42062
PixelGirl20 <105322502+PixelGirl20@users.noreply.github.com>
2025-03-13 10:46:18 +0100

    Update snmp-1.sh

--- snmp-1.sh
+++ snmp-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Définition des paramètres SNMP en dur
-oid="1.3.6.1.2.1.2.2.1.10.3"  # OID pour ifInOctets de l'interface 3
+oid="IF-MIB::ifHCOutOctets.3"  # OID pour ifInOctets de l'interface 3
 agent_ip="10.100.3.254"         # Adresse IP de l'agent SNMP
 community="123test123"             # Communauté SNMP v2c
 
